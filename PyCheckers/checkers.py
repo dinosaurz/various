fd2222ac4705f43eba2e9a3032ae442de40b49b7
David Karwowski <david.gitmail@gmail.com>
2012-12-31 01:10:32 -0500

    added event handling

--- PyCheckers/checkers.py
+++ PyCheckers/checkers.py
@@ -30,6 +30,15 @@ class DrawableCheckers(QtGui.QWidget):
         self.squaresize = squaresize
         self.piecesize = piecesize
 
+        # selection information
+        self.select_col = None
+        self.select_row = None
+        self.first_selected = False
+        self.first_col = None
+        self.first_row = None
+        self.select_border_color = 'white'
+        self.select_outline = 2
+
         self.piecepad = squaresize - piecesize
         self.piece_border_color = 'black'
         self.pieceoutline = 2
@@ -75,6 +84,7 @@ class DrawableCheckers(QtGui.QWidget):
         self._draw_sink(painter)
 
         self._draw_all_squares(painter)
+        self._draw_selections(painter)
 
     def _draw_sink(self, painter):
         sink_pen = QtGui.QPen(QtGui.QColor(self.sinkcolor))
@@ -153,5 +163,114 @@ class DrawableCheckers(QtGui.QWidget):
                             QtGui.QBrush(color, QtCore.Qt.SolidPattern))
         painter.drawEllipse(piece_rect)
 
+    def _draw_selection(self, painter):
+        select_pen = QtGui.QPen(QtGui.QColor(self.select_border_color))
+        select_pen.setWidth(self.select_outline)
+        painter.setPen(select_pen)
+
+        select_size = self.piecesize + self.select_outline
+        select_rect = None
+        col, row = select_col, select_row
+
+        if row and col:
+            select_rect = QtCore.QRect(self.sinkwidth + col * select_size + 1,
+                                       self.sinkwidth + row * select_size + 1,
+                                       select_size, select_size)
+        elif row:
+            select_rect = QtCore.QRect(self.sinkwidth + 1,
+                                       self.sinkwidth + row * select_size + 1,
+                                       select_size, select_size * self.ncols)
+        elif col:
+            select_rect = QtCore.QRect(self.sinkwidth + col * select_size + 1,
+                                       self.sinkwidth + 1,
+                                       select_size * self.nrows, select_size)
+        else:
+            return
+
+        self.drawRect(select_rect)
+
+    def _clear_selections(self):
+        if self.select_row or self.select_col:
+            self.select_row = None
+            self.select_col = None
+        else:
+            self.first_selected = False
+            self.first_col = None
+            self.first_row = None
+
+    def _enter_selections(self):
+        self._clear_selections()
+        if not all([self.select_col, self.select_row]):
+            return
+
+        if self.first_selected:
+            orig = (self.first_col, self.first_row)
+            land = (self.select_col, self.select_row)
+            if self.select_row - self.first_row == 2:
+                over = (self.select_col - self.first_col,
+                        self.select_row - self.select_col)
+                self.board.jump(orig, over, land)
+            else:
+                self.board.jump(orig, land)
+        else:
+            self.first_selected = True
+            self.first_col = self.select_col
+            self.first_row = self.select_row
+
+    def _select_col(self, col):
+        self.select_col = col
+
+    def _select_row(self, row):
+        self.select_row = row
+
 # - end drawing functions -----------------------------------------------------
+
+
+class MainCheckers(DrawableCheckers):
+    """ The main checkers window, with pieces that can be moved through
+        clicking on the piece and selecting a legal move to change to.
+        Added support will create a "Legal move" feature to highlight
+        choices of landings.
+    """
+    def __init__(self, parent, nrows, ncols, squaresize):
+        super(MainCheckers, self).__init__(parent, nrows, ncols, squaresize)
+
+        # Dictionary to decide what to do based on key presses
+        self.general_keys = {QtCore.Qt.Key_Escape: self._clear_selections,
+                             QtCore.Qt.Key_Enter:  self._enter_selections}
+        self.col_keys = {QtCore.Qt.Key_A: 0, QtCore.Qt.Key_B: 1,
+                         QtCore.Qt.Key_C: 2, QtCore.Qt.Key_D: 3,
+                         QtCore.Qt.Key_E: 4, QtCore.Qt.Key_F: 5,
+                         QtCore.Qt.Key_G: 6, QtCore.Qt.Key_H: 7}
+        self.row_keys = {QtCore.Qt.Key_1: 0, QtCore.Qt.Key_2: 1,
+                         QtCore.Qt.Key_3: 2, QtCore.Qt.Key_4: 3,
+                         QtCore.Qt.Key_5: 4, QtCore.Qt.Key_6: 5,
+                         QtCore.Qt.Key_7: 6, QtCore.Qt.Key_8: 7}
+
+# - start control functions ---------------------------------------------------
+
+    def restart(self):
+        self.board = CheckersBoard(self.nrows, self.ncols)
+        self.update()
+
+    def keyPressEvent(self, event):
+        if event.key() in self.general_keys:
+            self.general_keys[event.key()]()
+        elif event.key() in self.col_keys:
+            self._select_col(self.col_keys[event.key()])
+        elif event.key() in self.row_keys:
+            self._select_row(self.row_keys[event.key()])
+        else:
+            return
+
+        self.update()
+
+    def mousePressEvent(self, event):
+        col = event.x() // self.width
+        row = event.y() // self.height
+
+        self._select_col(col)
+        self._select_row(row)
+
+        self.update()
 
